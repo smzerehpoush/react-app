974beb2b8603f2229fd7c0e4fcb8fe960d19907f
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 18:02:44 +0330

    comment out the code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,28 +18,35 @@ class Counter extends Component {
     }));
   };
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
     return (
-      <React.Fragment>
-        <span style={this.styles} className={classes}>
-          {this.formatCount()}
-        </span>
-        <button
-          onClick={() => this.handleIncrement()}
-          className="btn btn-secondary btn-sm m-2"
-        >
-          Increment
-        </button>
-        <button
-          onClick={this.handleDecrement}
-          className="btn btn-secondary btn-sm m-2"
-        >
-          Decrement
-        </button>
-      </React.Fragment>
+      <main className="container">
+        <h1>Hello World</h1>
+      </main>
     );
   }
+  // render() {
+  //   let classes = "badge m-2 badge-";
+  //   classes += this.state.count === 0 ? "warning" : "primary";
+  //   return (
+  //     <React.Fragment>
+  //       <span style={this.styles} className={classes}>
+  //         {this.formatCount()}
+  //       </span>
+  //       <button
+  //         onClick={() => this.handleIncrement()}
+  //         className="btn btn-secondary btn-sm m-2"
+  //       >
+  //         Increment
+  //       </button>
+  //       <button
+  //         onClick={this.handleDecrement}
+  //         className="btn btn-secondary btn-sm m-2"
+  //       >
+  //         Decrement
+  //       </button>
+  //     </React.Fragment>
+  //   );
+  // }
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
