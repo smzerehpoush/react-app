cb5f00b832e5446635b7643c7e87d3a99ef9b4d6
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 18:02:28 +0330

    add new Component : movie

---
+++ src/components/movie.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+class Movie extends Component {
+  state = {};
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th Title />
+            <th Genre />
+            <th Stock />
+            <th Rate />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movie;
