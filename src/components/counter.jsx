66129f17c76b2a458d0332eddaed0ff9d040bf4d
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 22:13:10 +0330

    uncomment code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: this.props.value || 0
   };
   styles = {
     fontSize: 20,
@@ -17,36 +17,32 @@ class Counter extends Component {
       count: prev.count > 0 ? prev.count - 1 : prev.count
     }));
   };
+
   render() {
+    let classes = "badge m-2 badge-";
+    console.log(this.props);
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
-      <main className="container">
-        <h1>Hello World</h1>
-      </main>
+      <React.Fragment>
+        <span style={this.styles} className={classes}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+        <br />
+      </React.Fragment>
     );
   }
-  // render() {
-  //   let classes = "badge m-2 badge-";
-  //   classes += this.state.count === 0 ? "warning" : "primary";
-  //   return (
-  //     <React.Fragment>
-  //       <span style={this.styles} className={classes}>
-  //         {this.formatCount()}
-  //       </span>
-  //       <button
-  //         onClick={() => this.handleIncrement()}
-  //         className="btn btn-secondary btn-sm m-2"
-  //       >
-  //         Increment
-  //       </button>
-  //       <button
-  //         onClick={this.handleDecrement}
-  //         className="btn btn-secondary btn-sm m-2"
-  //       >
-  //         Decrement
-  //       </button>
-  //     </React.Fragment>
-  //   );
-  // }
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
