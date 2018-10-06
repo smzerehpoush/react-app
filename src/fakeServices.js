207c72f13a824264df15c22500ac8e546de5bf5f
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 18:02:13 +0330

    add new file : fakeServices ...

---
+++ src/fakeServices.js
@@ -0,0 +1,44 @@
+const movies = [
+  {
+    _id: 1,
+    title: "test title",
+    genre: "test genre",
+    numberInStock: 6,
+    dailyRentalRate: 2.5
+  },
+  {
+    _id: 2,
+    title: "test title",
+    genre: "genre",
+    numberInStock: 6,
+    dailyRentalRate: 2.5
+  },
+  {
+    _id: 3,
+    title: "test title",
+    genre: "test genre",
+    numberInStock: 6,
+    dailyRentalRate: 2.5
+  },
+  {
+    _id: 4,
+    title: "test title",
+    genre: "test genre",
+    numberInStock: 6,
+    dailyRentalRate: 2.5
+  },
+  {
+    _id: 5,
+    title: "test title",
+    genre: "test genre",
+    numberInStock: 6,
+    dailyRentalRate: 2.5
+  },
+  {
+    _id: 6,
+    title: "test title",
+    genre: "test genre",
+    numberInStock: 60,
+    dailyRentalRate: 2
+  }
+];
