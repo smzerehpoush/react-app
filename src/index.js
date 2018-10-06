c5219b87417fe22609cde1e49259be7c8ea3ae66
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 22:13:43 +0330

    render counters instead of movies component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import './index.css'
-// import App from './App'
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
-import Movie from "./components/movie";
-ReactDOM.render(<Movie />, document.getElementById("root"));
+// import Movie from "./components/movie";
+import Counters from "./components/counters"
+ReactDOM.render( < Counters / > , document.getElementById("root"));
