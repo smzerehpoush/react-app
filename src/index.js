5214cf52ac7f369f2c25aee27572e61251c736cf
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-06 18:01:51 +0330

    prettify file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import './index.css'
 // import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter'
-ReactDOM.render(<Counter/>, document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+import Movie from "./components/movie";
+ReactDOM.render(<Movie />, document.getElementById("root"));
