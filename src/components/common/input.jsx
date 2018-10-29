8b35b74b4ffcfffef02992811d8747111d216769
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-29 19:13:55 +0330

    change input component

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-const Input = ({ name, label, value, onChange, errorMessage, type }) => {
+
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type ? type : "text"}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="form-control"
-        id={name}
-      />
-      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-      {/* <small className="form-text text-muted">{errorMessage}</small> */}
+      <input {...rest} name={name} id={name} className="form-control" />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
