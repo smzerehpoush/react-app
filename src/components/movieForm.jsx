24e1436f3108d9ddcbf72345fe6128ca4be4ca24
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-30 13:56:06 +0330

    fix component

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,18 +1,96 @@
-import React, { Component } from "react";
-class MovieForm extends Component {
-  handleSave = () => {
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import Select from "react-select";
+import { getNewMovieId, getMovie } from "../services/fakeMovieServices";
+
+class MovieForm extends Form {
+  state = {
+    data: { title: "", stock: "", rate: "", genre: "" },
+    errors: {}
+  };
+
+  schema = {
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genre: Joi.string()
+      .required()
+      .label("Gnere"),
+    stock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number In Stock"),
+    rate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Rate")
+  };
+
+  doSubmit = () => {
+    const movieId = this.props.match.params.id;
+    const { data, selectedOption } = this.state;
+    const movie = {
+      _id: movieId || getNewMovieId(),
+      title: data.title,
+      name: data.title,
+      genre: data.genre || selectedOption.label,
+      liked: "false",
+      numberInStock: data.stock,
+      dailyRentalRate: data.rate
+    };
+    console.log("movie to be add ", movie);
+    const { onAddMovie } = this.props;
+    onAddMovie(movie);
     this.props.history.push("/movies");
   };
+  handleSelectChange = selectedOption => {
+    const data = { ...this.state.data };
+    console.log("selectedOption", selectedOption);
+    data.genre = selectedOption.label;
+
+    this.setState({ selectedOption, data });
+  };
+  componentDidMount() {
+    const movieId = this.props.match.params.id;
+    if (movieId) {
+      const movie = getMovie(movieId);
+      const data = { ...this.state.data };
+      data.title = movie.title;
+      data.stock = movie.numberInStock;
+      data.genre = movie.genre;
+      data.rate = Math.ceil(movie.dailyRentalRate);
+      this.setState({ data });
+    }
+  }
   render() {
+    const { selectedOption } = this.state;
+    const { genres } = this.props;
+    const options = genres.map(genre => ({
+      value: genre._id,
+      label: genre.genre
+    }));
+
     return (
-      <div>
-        <h1> Movie {this.props.match.params.id}</h1>
-        <button
-          className="btn btn-secondary btn-sm m-2"
-          onClick={this.handleSave}
-        >
-          save
-        </button>
+      <div className="m-4">
+        <h1>Add Movie</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          <p>Genre</p>
+          <Select
+            name="genre"
+            value={selectedOption}
+            onChange={this.handleSelectChange}
+            options={options}
+            placeholder="Genre"
+          />
+          <br />
+          {this.renderInput("stock", "Number In Stock", "number")}
+          {this.renderInput("rate", "Rate", "number")}
+          {this.renderButton("Save")}
+        </form>
       </div>
     );
   }
