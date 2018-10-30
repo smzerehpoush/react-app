c337c90a9a5961629ac8cc600ae8593f07fc50d6
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-30 11:36:36 +0330

    refactor and add new method

--- src/services/fakeGenreServices.js
+++ src/services/fakeGenreServices.js
@@ -1,16 +1,20 @@
-const genres = [{
-        _id: 1,
-        genre: "genre1"
-    },
-    {
-        _id: 2,
-        genre: "genre2"
-    },
-    {
-        _id: 3,
-        genre: "genre3"
-    }
-]
+const genres = [
+  {
+    _id: 1,
+    genre: "genre1"
+  },
+  {
+    _id: 2,
+    genre: "genre2"
+  },
+  {
+    _id: 3,
+    genre: "genre3"
+  }
+];
 export function getGenres() {
-    return genres
+  return genres;
+}
+export function getGenre(id) {
+  return genres.find(m => m._id === id);
 }
