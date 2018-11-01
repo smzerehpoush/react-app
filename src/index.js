6008f295feeff8fbd9b3910df4d768da48af9732
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-11-01 13:23:35 +0330

    fix merge bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
